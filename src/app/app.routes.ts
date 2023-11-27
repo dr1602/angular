b363c9c130fd90035a51148d5505692014853b9a
dr1602 <david.arangelg@gmail.com>
2023-11-27 13:58:46 -0600

    Primer Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component'
+import { LabsComponent } from './pages/labs/labs.component'
+
+export const routes: Routes = [
+  {
+    path: '', // para que vaya a home desde el path inicial
+    component: HomeComponent
+  },
+  {
+    path: 'labs',
+    component: LabsComponent
+  }
+];
